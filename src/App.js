0a57adaf71241467c5a33686da52cecb8deed6bf
Pav125 <devipavan824@gmail.com>
2024-05-12 08:25:53 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [todos, updateTodo] = useState([
+    {
+      id: 0,
+      text: 'Buy milk',
+    },
+    {
+      id: 1,
+      text: 'Buy eggs',
+    },
+  ])
+  const [todoInput, updateInput] = useState('')
+
+  const generateId = () => {
+    const maxId = todos.reduce((max, todo) => (max < todo.id ? todo.id : max), 0);
+    return maxId + 1;
+  }
+
+  const deleteTodo = (id)=>{
+    console.log("Deleted a task");
+    updateTodo(todos.filter(todo=>todo.id!==id))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div>
+        <input type='text' value={todoInput} onChange={(e) => {
+          updateInput(e.target.value);
+        }} />
+        <button onClick={() => {
+          console.log("Added a new task");
+          const newId = generateId();
+          updateTodo([
+            ...todos,
+            {
+              id: newId,
+              text: todoInput,
+            }
+            
+          ]);
+          updateInput('');
+        }} >Add</button>
+      </div>
+      <div>
+        <ul>
+          {
+            todos.map((todo) => {
+              return (
+                <div key={todo.id}>
+                  <li>{todo.id} : {todo.text}</li>
+                  <button onClick={()=>{
+                    deleteTodo(todo.id)
+                  }} >❌</button>
+                </div>
+              )
+              
+            })
+          }
+        </ul>
+      </div>
+    </>
+  )
+
 }
 
-export default App;
+export default App
